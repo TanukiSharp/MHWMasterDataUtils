acfbb27d79ab98d99cc707eb2a5038532c176b67
Sebastien ROBERT <sebastien.saigo@gmail.com>
2019-07-04 00:31:29 +0900

    SharpnessPackageProcessor code cleanup

--- MHWMasterDataUtils/Sharpness/SharpnessPackageProcessor.cs
+++ MHWMasterDataUtils/Sharpness/SharpnessPackageProcessor.cs
@@ -8,14 +8,17 @@ namespace MHWMasterDataUtils.Sharpness
 {
     public class SharpnessPackageProcessor : PackageProcessorBase
     {
-        public override bool IsChunkFileMatching(string chunkFullFilename)
-        {
-            return chunkFullFilename == @"\common\equip\kireaji.kire";
-        }
+        public const ushort HeaderValue = 0x0177;
+        public Dictionary<uint, SharpnessInfo> Table { get; } = new Dictionary<uint, SharpnessInfo>();
 
-        public const ushort HeaderIdentifier = 0x0177;
-
-        public readonly Dictionary<uint, SharpnessInfo> Table = new Dictionary<uint, SharpnessInfo>();
+        private const int Id = 0;
+        private const int Red = 1;
+        private const int Orange = 2;
+        private const int Yellow = 3;
+        private const int Green = 1;
+        private const int Blue = 1;
+        private const int White = 1;
+        private const int Purple = 1;
 
         public override Task PreProcess()
         {
@@ -23,26 +26,57 @@ namespace MHWMasterDataUtils.Sharpness
             return base.PreProcess();
         }
 
+        public override bool IsChunkFileMatching(string chunkFullFilename)
+        {
+            return chunkFullFilename == @"\common\equip\kireaji.kire";
+        }
+
+        private uint ReadHeader(Reader reader)
+        {
+            ushort headerValue = reader.ReadUInt16();
+
+            if (headerValue != HeaderValue)
+                throw new FormatException($"Invalid header in file '{reader.Filename ?? "<unknown>"}'. Expected {HeaderValue:x4}, read {headerValue:x4}.");
+
+            return reader.ReadUInt32();
+        }
+
+        private static void ReadValues(Reader reader, ushort[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+                elements[i] = reader.ReadUInt16();
+        }
+
+        public void TryAddSharpnessValues(ushort[] sharpnessValues)
+        {
+            if (Table.ContainsKey(sharpnessValues[Id]))
+                return;
+
+            var sharpnessInfo = new SharpnessInfo(
+                sharpnessValues[Red],
+                sharpnessValues[Orange],
+                sharpnessValues[Yellow],
+                sharpnessValues[Green],
+                sharpnessValues[Blue],
+                sharpnessValues[White],
+                sharpnessValues[Purple]
+            );
+
+            Table.Add(sharpnessValues[0], sharpnessInfo);
+        }
+
         public override Task ProcessChunkFile(Stream stream, string chunkFullFilename)
         {
+            ushort[] sharpnessValues = new ushort[8];
+
             using (var reader = new Reader(new BinaryReader(stream, Encoding.UTF8, true), chunkFullFilename))
             {
-                ushort identifier = reader.ReadUInt16();
+                uint numEntries = ReadHeader(reader);
 
-                if (identifier != HeaderIdentifier)
-                    throw new FormatException($"Invalid header identifier in file '{reader.Filename ?? "<unknown>"}'. Expected {HeaderIdentifier:x4}, read {identifier:x4}.");
-
-                uint num_entries = reader.ReadUInt32();
-
-                ushort[] elements = new ushort[8];
-
-                for (uint i = 0; i < num_entries; i++)
+                for (uint i = 0; i < numEntries; i++)
                 {
-                    for (int j = 0; j < elements.Length; j++)
-                        elements[j] = reader.ReadUInt16();
-
-                    if (Table.ContainsKey(elements[0]) == false)
-                        Table.Add(elements[0], new SharpnessInfo(elements[1], elements[2], elements[3], elements[4], elements[5], elements[6], elements[7]));
+                    ReadValues(reader, sharpnessValues);
+                    TryAddSharpnessValues(sharpnessValues);
                 }
             }
 
